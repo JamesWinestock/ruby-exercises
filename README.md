46e11708bdde90618098664a5a9c5892c714689a
James Winestock <james.winestock@gmail.com>
2016-04-14 14:54:23 +1000

    Added readme and some basic calculator functions

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # ruby-exercises
-My first repository of basic Ruby exercises. 
-Also the first repository I succesfully put to git-hub.
+My first repository of basic Ruby exercises.
+Also the first repository I successfully put to git-hub.
--- calculator.rb
+++ calculator.rb
@@ -9,6 +9,7 @@ def subtract(a,b)
 end
 
 def multiply(a,b)
+  a * b
   # put your code here
 end
 
@@ -19,11 +20,21 @@ end
 puts "Welcome to RubyCalc"
 print "Enter operation (+, -, *, /): "
 # enter your code here
+operation = gets.chomp
 print "Enter first number: "
+a = gets.chomp
 # enter your code here
 print "Enter second number: "
+b = gets.chomp
 # enter your code here
 
 # do calculation
+if operation == "+"
+  puts add(a,b)
+elsif operation == "-"
+  subtract
+elsif operation == "*"
+  multiply
+end
 
 puts "The result is: #{}"
